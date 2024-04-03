006b9ac41e2c71cdef6df5cc2546e2b0243f4f42
Matthijs van Otterdijk <matthijs@terminusdb.com>
2024-04-03 14:39:22 +0200

    spawn hardcoded machine types into hardcoded test security groups

--- spawn_machine.py
+++ spawn_machine.py
@@ -1,6 +1,9 @@
 from libcloud.compute.types import Provider
 from libcloud.compute.providers import get_driver
 import boto3
+from paramiko import AutoAddPolicy
+from paramiko.client import SSHClient
+from paramiko.ssh_exception import NoValidConnectionsError
 
 session = boto3.Session()
 credentials = session.get_credentials()
@@ -11,4 +14,58 @@ secret_key = credentials.secret_key
 cls = get_driver(Provider.EC2)
 driver = cls(access_key, secret_key)
 
-print(driver.list_nodes())
+IMAGE_ID = "ami-0c2f5dff1fc37a2bf"
+#SIZE_ID = "g5.xlarge"
+SIZE_ID = "t3.micro"
+SUBNET ="subnet-0141736fba5a518b1"
+SG="sg-001bc2fe593af960e"
+
+print('listing sizes..')
+sizes = driver.list_sizes()
+size = [s for s in sizes if s.id == SIZE_ID][0]
+print(size)
+print('get subnet..')
+subnet = [ s for s in driver.ex_list_subnets() if s.id == SUBNET][0]
+print(subnet)
+print('get image..')
+image = driver.get_image(IMAGE_ID)
+
+print(image)
+
+node = driver.create_node(name="test-vectorize-autospawn",
+                          image=image,
+                          size=size,
+                          ex_assign_public_ip=True,
+                          ex_subnet=subnet,
+                          ex_terminate_on_shutdown=True,
+                          ex_security_group_ids=[SG])
+print('started!!')
+print(node)
+
+[(node, [ip])] = driver.wait_until_running([node], wait_period=5, timeout=600)
+print(f'running!! {node} {ip}')
+
+
+def get_ssh_conn():
+    ssh_client = SSHClient()
+    ssh_client.set_missing_host_key_policy(AutoAddPolicy())
+    while True:
+        try:
+            ssh_client.connect(ip, username="ubuntu", timeout=1.0, )
+            return ssh_client
+        except TimeoutError:
+            print('timeout.. retry')
+            pass
+        except NoValidConnectionsError:
+            print('no valid connections.. retry')
+            pass
+
+
+ssh_client = get_ssh_conn()
+_stdin, _stdout, _stderr = ssh_client.exec_command('cat /proc/cpuinfo')
+print(_stdout.read().decode())
+ssh_client.close()
+
+
+print(driver.destroy_node(node))
+print('ded')
