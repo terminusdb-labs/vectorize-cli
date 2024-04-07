968f957c1a5b710a15c55627e5cb71e94b3c5e3e
Matthijs van Otterdijk <matthijs@terminusdb.com>
2024-04-07 22:35:22 +0200

    try resuming

--- vectorize_etcd.py
+++ vectorize_etcd.py
@@ -9,10 +9,7 @@ import os
 def retrieve_identity():
     return socket.getfqdn()
 
-def start_(task):
-    print('really gonna start now!')
-    task.start()
-    print('started!')
+def start_(task, truncate=None, skip=0):
     init = task.init()
     input_file = init['input_file']
     output_file = init['output_file']
@@ -23,7 +20,16 @@ def start_(task):
     chunk = []
     count = 0
     with open(output_file, 'w') as output_fp:
+        # truncate to a safe known size
+        if truncate:
+            output_file.truncate(truncate)
+
         with open(input_file, 'r') as input_fp:
+            # skip already processed lines
+            if skip != 0:
+                skip -= 1
+                continue
+
             for line in input_fp:
                 json_str = json.loads(line)
                 chunk.append(json_str)
@@ -42,16 +48,30 @@ def start_(task):
         os.fsync(output_fp.fileno())
         task.finish(count)
 
-def start(task):
+def start(task, truncate=None,skip=0):
+    task.start()
     try:
-        start_(task)
+        start_(task, truncate=truncate, skip=skip)
     except TaskInterrupted as e:
         pass
     except Exception as e:
         task.finish_error(str(e))
 
 def resume(task):
-    pass
+    # We have to figure out where we left off
+    # This is determined by the current file size. rounding that down
+    # to the nearest multiple of the vector size gets us a reliable
+    # count. This might be lower than the number in progress!
+    if task.status() == 'paused':
+        task.resume()
+    init = task.init()
+    size = os.path.getsize(init['output_file'])
+    count = size // 4096
+    truncate_to = count * 4096
+
+    print(f'resuming after having already vectorized {count}')
+
+    start(task, truncate=truncate_to, skip=count)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -76,6 +96,8 @@ if __name__ == '__main__':
                 print('starting..')
                 start(task)
             case 'running':
+                resume(task)
+            case 'paused':
                 resume(task)
             case _:
                 sys.stderr.write(f'cannot process task with status {task.status()}\n')
